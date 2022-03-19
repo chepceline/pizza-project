e411e682f5d9281de489caa6893c142d27b4e418
chepceline <chepceline25@gmail.com>
2022-03-19 16:03:16 +0300

    initial

---
+++ js/scripts.js
@@ -0,0 +1,12 @@
+function getSizeCost(){
+    var selectedSize= document.getElementById("size").value;
+    return parseInt(selectedSize);
+}
+function getCrustCost(){
+    var selectedCrust= document.getElementById("crust").value;
+    return parseInt(selectedCrust);
+}
+function getNumber(){
+    var selectedNumber= document.getElementById("numberofpizza").value;
+    return parseInt(selectedNumber);
+}
