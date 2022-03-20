af9648380ed24761d7fd18702160559f8bcab5bb
chepceline <chepceline25@gmail.com>
2022-03-20 12:05:23 +0300

    initial

--- js/scripts.js
+++ js/scripts.js
@@ -10,3 +10,26 @@ function getNumber(){
     var selectedNumber= document.getElementById("numberofpizza").value;
     return parseInt(selectedNumber);
 }
+function mushrooms(){
+    var mushroom = 0;
+    var addMushroom = document.getElementById("toppingone");
+    if(addMushroom.checked == true);{
+        mushroom = 150
+    } return parseInt (mushroom);
+
+}
+function sausage(){
+    var sausage = 0;
+    var addSausage = document.getElementById("toppingone");
+    if(addSausage.checked == true);{
+        sausage = 140
+    } return parseInt (sausage);
+}
+function onions(){
+    var onions = 0;
+    var addOnions = document.getElementById("toppingone");
+    if(addOnions.checked == true);{
+        onion = 130
+    } return parseInt (onion);
+}
+
